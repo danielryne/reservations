35d5ceeb559769abe076b9f7d0ec59833379967b
Daniel Lucio <Daniel@Daniels-MacBook-Air-2.local>
2017-10-28 11:25:49 -0500

    first draft of program

---
+++ server.js
@@ -0,0 +1,112 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Intial data 
+// =============================================================
+var reservations = [];
+
+var tables = [
+  {
+    name: "Daniel",
+    number: "555-555-5555",
+    email: "daniel@fakemail.com",
+    ID: 105 
+  },
+  {
+    name: "Forrest",
+    number: "444-444-4444",
+    email: "forrest@fakemail.com",
+    ID: 104
+  },
+  {
+    name: "Miguel",
+    number: "333-333-3333",
+    email: "miguel@fakemail.com",
+    ID: 103
+  },
+  {
+    name: "Sharad",
+    number: "222-222-2222",
+    email: "sharad@fakemail.com",
+    ID: 102
+  }
+];
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+
+// Returns 
+app.get("/api/tables", function(req, res) {
+  var chosen = req.params.characters;
+
+  return res.json(tables);
+});
+
+// Create reservations (takes in JSON input) 
+app.post("/api/tables", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body-parser middleware
+  var newReservation = req.body;
+
+  //verifying what was sent from the html
+  console.log(newReservation);
+
+  // Sees if there are tables available 
+  if (tables.length <= 6){
+
+    //puts new reservation in the tables array 
+    tables.push(newReservation);
+
+    //If the table was reserved, sends back true 
+    res.json(true);
+  }
+  else{
+
+    //puts new reservation in the tables array 
+    reservations.push(newReservation);
+
+    // If there are too many reservations, send back false 
+    res.json(false); 
+  }
+
+});
+
+// Clears the data in the table 
+app.post("/api/clear", function(req, res) {
+
+  reservations = [];
+  tables = [];
+
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
